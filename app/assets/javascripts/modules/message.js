361b74148e1c0c38b9a0ddc046866d904814b628
yamanaka-takuya <taku.1031.kun@icloud.com>
2020-08-25 11:58:29 +0900

    自動更新機能の実装

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
     function buildHTML(message){
       if ( message.image ) {
         let html =
-          `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
             <div class="MessageInfo">
               <div class="MessageInfo__userName">
                 ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
         return html;
       } else {
         let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -60,9 +60,9 @@ $(function(){
       })
       .fail(function() {
         alert("メッセージ送信に失敗しました");
-    })
+      })
       .always(function(){
         $('.Form__submitBtn').prop('disabled', false);
+      });
     });
-  });
 })
