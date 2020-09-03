24739da92ac9326ab9da23178bc3b4447a4c8953
yamanaka-takuya <taku.1031.kun@icloud.com>
2020-09-03 18:17:20 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# working_directory app_path
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
